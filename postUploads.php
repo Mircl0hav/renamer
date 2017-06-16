191a1367a4603fa3f921775c1936c2acf12a939b
jadam <pro@jonathan-adam.fr>
2017-06-16 13:30:31 +0200

    add debug conf

--- postUploads.php
+++ postUploads.php
@@ -4,16 +4,6 @@ use Tools\Renamer;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$logger = new \Monolog\Logger('renamer');
-
-$logHandler = new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::ERROR);
-$logHandler->setFormatter(new \Bramus\Monolog\Formatter\ColoredLineFormatter());
-$logger->pushHandler($logHandler);
-
-$logger->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . DIRECTORY_SEPARATOR . 'logs/' . date('Y-m-d'),
-    \Monolog\Logger::ERROR));
-$logHandler->setFormatter(new \Monolog\Formatter\LineFormatter(null, null, true, true));
-
 $args = $argv;
 $opts = getopt('dec:');
 
@@ -27,6 +17,7 @@ if (!empty($opts['c'])) {
     $excludes_path = $config->excludes;
     $size = (array)$config->size;
     $keep = $config->keep;
+    $debug = !empty($config->debug) ? \Monolog\Logger::DEBUG : \Monolog\Logger::ERROR;
     unset($config);
 } elseif (!empty($args['1']) && !empty($args['2'])) {
     echo "usage : php postUploads.php SOURCE DEST\r\n";
@@ -46,6 +37,17 @@ if (!empty($opts['c'])) {
         $excludes_path = explode(':', $opts['e']);
     }
 }
+
+$logger = new \Monolog\Logger('renamer');
+
+$logHandler = new \Monolog\Handler\StreamHandler('php://stdout', \Monolog\Logger::ERROR);
+$logHandler->setFormatter(new \Bramus\Monolog\Formatter\ColoredLineFormatter());
+$logger->pushHandler($logHandler);
+
+$logger->pushHandler(new \Monolog\Handler\StreamHandler(__DIR__ . DIRECTORY_SEPARATOR . 'logs/' . date('Y-m-d'),
+    $errorLvl));
+$logHandler->setFormatter(new \Monolog\Formatter\LineFormatter(null, null, true, true));
+
 $renamer = (new Renamer($source, $destination))
     ->setLogger($logger)
     ->setExcludedPath($excludes_path)
